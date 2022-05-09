4af395a1fea4909050e19473a046774fd1ebffaf
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-09 17:14:28 -0600

    Added validation for smaller than 999999999

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Screen({ value }) {
     let screenContent = 'ERROR'
 
-    if (parseInt(value, 10) >= 0) {
+    if (parseInt(value, 10) >= 0 && parseInt(value, 10) <= 999999999 ) {
         screenContent = value
-    }
+    } 
 
     return (
         <input type="text" className="screen" value={screenContent} readOnly />
