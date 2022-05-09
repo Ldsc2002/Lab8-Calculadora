e2d8e3513765d979d8a98ed5150d02a73690f87b
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-09 17:04:50 -0600

    Display error when number is negative

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function Screen({ value }) {
+    let screenContent = 'ERROR'
+
+    if (parseInt(value, 10) >= 0) {
+        screenContent = value
+    }
+    
     return (
-        <input type="text" className="screen" value={value} readOnly />
+        <input type="text" className="screen" value={screenContent} readOnly />
     )
 }
 
