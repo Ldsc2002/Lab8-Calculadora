4499b1050c13a27cf3c226b3948204f00299e8bd
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-09 17:38:51 -0600

    Linting

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -5,7 +5,7 @@ function Screen({ value }) {
 
     if (parseFloat(value, 10) >= 0 && parseFloat(value, 10) <= 999999999 && value.length < 10) {
         screenContent = value
-    } 
+    }
 
     return (
         <input type="text" className="screen" value={screenContent} readOnly />
