bec97948f9340cf0e8472fb59c02e45112e0b5f5
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-09 17:02:35 -0600

    Working basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FunctionButton from './components/FunctionButton'
 import NumberButton from './components/NumberButton'
 import OperatorButton from './components/OperatorButton'
 import Screen from './components/Screen'
 
 function App() {
+    const [dataDisplay, setDisplay] = useState(0)
+    const [operation, setOperation] = useState('')
+    const [prevNumber, setPrevNumber] = useState(0)
+    const [numData, setNumData] = useState([])
+
+    const setData = (value) => {
+        let newData = []
+
+        if (numData.length < 9) {
+            newData = numData
+            newData.push(value)
+            setNumData([...newData])
+            setDisplay(numData.join(''))
+        }
+    }
+
+    const operationHandler = (value) => {
+        setPrevNumber(dataDisplay)
+        setNumData([...[]])
+        setDisplay(0)
+        setOperation(value)
+    }
+
+    const functionHandler = (value) => {
+        let result = 0
+        if (value === '=') {
+            if (operation === '+') result = parseInt(prevNumber, 10) + parseInt(dataDisplay, 10)
+            else if (operation === '-') result = parseInt(prevNumber, 10) - parseInt(dataDisplay, 10)
+            else if (operation === 'x') result = parseInt(prevNumber, 10) * parseInt(dataDisplay, 10)
+            else if (operation === '/') result = parseInt(prevNumber, 10) / parseInt(dataDisplay, 10)
+            setDisplay(result)
+        } else if (value === 'AC') {
+            setDisplay(0)
+            setNumData([...[]])
+            setOperation('')
+            setPrevNumber(0)
+        }
+    }
+
     return (
         <div>
             <div className="calculator">
-                <Screen value="Testing" />
+                <Screen value={dataDisplay} />
 
                 <div className="button-container">
-                    <OperatorButton value="/" />
-                    <OperatorButton value="x" />
-                    <OperatorButton value="+" />
-                    <OperatorButton value="-" />
+                    <OperatorButton value="/" setButton={operationHandler} />
+                    <OperatorButton value="x" setButton={operationHandler} />
+                    <OperatorButton value="+" setButton={operationHandler} />
+                    <OperatorButton value="-" setButton={operationHandler} />
 
-                    <NumberButton value="7" />
-                    <NumberButton value="8" />
-                    <NumberButton value="9" />
+                    <NumberButton value="7" setButton={setData} />
+                    <NumberButton value="8" setButton={setData} />
+                    <NumberButton value="9" setButton={setData} />
 
-                    <NumberButton value="4" />
-                    <NumberButton value="5" />
-                    <NumberButton value="6" />
+                    <NumberButton value="4" setButton={setData} />
+                    <NumberButton value="5" setButton={setData} />
+                    <NumberButton value="6" setButton={setData} />
 
-                    <NumberButton value="1" />
-                    <NumberButton value="2" />
-                    <NumberButton value="3" />
+                    <NumberButton value="1" setButton={setData} />
+                    <NumberButton value="2" setButton={setData} />
+                    <NumberButton value="3" setButton={setData} />
 
-                    <NumberButton value="0" />
-                    <NumberButton value="." />
+                    <NumberButton value="0" setButton={setData} />
+                    <NumberButton value="." setButton={setData} />
 
-                    <FunctionButton value="AC" />
-                    <FunctionButton value="=" />
+                    <FunctionButton value="AC" setButton={functionHandler} />
+                    <FunctionButton value="=" setButton={functionHandler} />
                 </div>
             </div>
         </div>
