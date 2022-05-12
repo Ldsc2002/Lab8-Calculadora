b0d0c0e6ff37214cc42974f68f1504a3d80e0dbb
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-12 14:48:16 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: "http://juanelcaballo.club/20226/Calculadora/"
 })
